c515f6a29e9ea56099809faea28474177592b3ef
R.Anthony <105434390+RingoetAnthony@users.noreply.github.com>
2024-05-31 13:05:57 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import CartProvider from "@/providers/CartProvider";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/nav/NavBar";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const metadata: Metadata = {
+  title: "E-Commerce",
+  description: "E-Commerce App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} text-slate-700`}>
+        <Toaster
+          toastOptions={{
+            style: {
+              background: "rgb(51 65 85)",
+              color: "#fff",
+            },
+          }}
+        />
+        <CartProvider>
+          <div className="flex flex-col min-h-screen">
+            <NavBar />
+            <main className="flex-grow">{children}</main>
+            <Footer />
+          </div>
+        </CartProvider>
+      </body>
+    </html>
+  );
+}
